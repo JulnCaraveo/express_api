36766ba18a5c6be56b6d64d1858185afda78c596
JulnCaraveo <alix_caraveo@hotmail.com>
2022-05-12 22:10:34 -0500

    mi prmer servidor api

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+//esto es un endpoint
+//routes
+//methods http: get,post,put,delete
+app.get('/v1/explorers',(req, res)=>{
+    console.log('get explorers v1 api ${new Date()}')
+    const explorer1={id:1,name:"explorer1"}
+    const explorer2={id:2,name:"explorer2"}
+    const explorers={id:3,name:"explorer3"}
+    //http code status: 200
+    res.status(200).json(explorers)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
